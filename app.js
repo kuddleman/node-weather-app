969262e9be26d7b520048e3b695b107c1d9a5a0c
Kuddleman <edl1155@gmail.com>
2020-05-15 10:16:10 -0700

    add request package

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-console.log('Starting')
+const request = require('request')
 
+const url = 'http://api.weatherstack.com/current?access_key=f25c9e4c1074ee45588aa6873d401edb&query=Phoenix'
 
-setTimeout(()=>{
-   console.log("2 second timer")
-}, 2000)
+//json: true is an option of the request package that
+//automatically parses the JSON data.  With this 'json' option,
+// we don't have to use the standard JSON.parse
 
-setTimeout(()=>{
-  console.log('0 second Timer')
-}, 0)
+request({ url: url, json: true }, (error, response)=>{
+ //console.log(response.body.current)
+  const weatherStats = response.body.current  
 
+  const currentTemp = weatherStats.temperature
 
+  const tempInFahrenheit = Math.floor((currentTemp * 9/5) + 32)
 
-console.log('Stopping')
+  const description = weatherStats.weather_descriptions[0]
+
+  console.log(`It is currently ${tempInFahrenheit} degrees out.  The weather is ${description}.`)
+  
+})
